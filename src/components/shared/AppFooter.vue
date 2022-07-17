522e1768659ba44605b5e354e6df538660e0b499
Vkbishu <bishukumar007@gmail.com>
2022-07-17 10:00:49 +0530

    social link updated in footer

--- src/components/shared/AppFooter.vue
+++ src/components/shared/AppFooter.vue
@@ -1,38 +1,9 @@
 <script>
 import feather from 'feather-icons';
 import FooterCopyright from './FooterCopyright.vue';
+import Social from "./Social.vue";
 export default {
-	components: { FooterCopyright },
-	data() {
-		return {
-			socials: [
-				{
-					id: 1,
-					name: 'GitHub',
-					icon: 'github',
-					url: 'https://github.com/realstoman',
-				},
-				{
-					id: 2,
-					name: 'Twitter',
-					icon: 'twitter',
-					url: 'https://twitter.com/realstoman',
-				},
-				{
-					id: 3,
-					name: 'Medium',
-					icon: 'book',
-					url: 'https://stoman.medium.com',
-				},
-				{
-					id: 4,
-					name: 'Instagram',
-					icon: 'instagram',
-					url: 'https://instagram.com/realstoman',
-				},
-			],
-		};
-	},
+	components: { FooterCopyright, Social },
 	mounted() {
 		feather.replace();
 	},
@@ -56,20 +27,7 @@ export default {
 				>
 					Follow me
 				</p>
-				<ul class="flex gap-4 sm:gap-8">
-					<a
-						v-for="social in socials"
-						:key="social.id"
-						:href="social.url"
-						target="__blank"
-						class="text-gray-400 hover:text-indigo-500 dark:hover:text-indigo-400 cursor-pointer rounded-lg bg-gray-50 dark:bg-ternary-dark hover:bg-gray-100 shadow-sm p-4 duration-500"
-					>
-						<i
-							:data-feather="social.icon"
-							class="w-5 sm:w-8 h-5 sm:h-8"
-						></i>
-					</a>
-				</ul>
+				<Social />
 			</div>
 
 			<!-- Footer copyright -->
