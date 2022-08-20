4d42dbbf664ec44782b2c97055545f7c007089b4
Vkbishu <bishukumar007@gmail.com>
2022-08-20 10:58:27 +0530

    remove project filter

--- src/components/projects/ProjectsGrid.vue
+++ src/components/projects/ProjectsGrid.vue
@@ -1,11 +1,10 @@
 <script>
 import feather from 'feather-icons';
-import ProjectsFilter from './ProjectsFilter.vue';
 import ProjectSingle from './ProjectSingle.vue';
 import projects from '../../data/projects';
 
 export default {
-	components: { ProjectSingle, ProjectsFilter },
+	components: { ProjectSingle },
 	data: () => {
 		return {
 			projects,
@@ -52,83 +51,12 @@ export default {
 	<!-- Projects grid -->
 	<section class="pt-10 sm:pt-14">
 		<!-- Projects grid title -->
-		<div class="text-center">
+		<div class="text-center mb-10 sm:mb-14">
 			<p
-				class="font-general-semibold text-2xl sm:text-5xl font-semibold mb-2 text-ternary-dark dark:text-ternary-light"
+				class="font-general-semibold text-3xl sm:text-4xl font-semibold mb-2 text-ternary-dark dark:text-ternary-light"
 			>
 				{{ projectsHeading }}
 			</p>
-		</div>
-
-		<!-- Filter and search projects -->
-		<div class="mt-10 sm:mt-10">
-			<h3
-				class="font-general-regular
-					text-center text-secondary-dark
-					dark:text-ternary-light
-					text-md
-					sm:text-xl
-					font-normal
-					mb-4
-				"
-			>
-				Search projects by title or filter by category
-			</h3>
-			<div
-				class="
-					flex
-					justify-between
-					border-b border-primary-light
-					dark:border-secondary-dark
-					pb-3
-					gap-2
-				"
-			>
-				<div class="flex justify-between gap-2">
-					<span
-						class="
-							hidden
-							sm:block
-							bg-primary-light
-							dark:bg-ternary-dark
-							p-2.5
-							shadow-sm
-							rounded-xl
-							cursor-pointer
-							"
-					>
-						<i
-							data-feather="search"
-							class="text-ternary-dark dark:text-ternary-light"
-						></i>
-					</span>
-					<input
-						v-model="searchProject"
-						class="font-general-medium
-						pl-3
-						pr-1
-						sm:px-4
-						py-2
-						border-1 border-gray-200
-						dark:border-secondary-dark
-						rounded-lg
-						text-sm
-						sm:text-md
-						bg-secondary-light
-						dark:bg-ternary-dark
-						text-primary-dark
-						dark:text-ternary-light
-						"
-						id="name"
-						name="name"
-						type="search"
-						required=""
-						placeholder="Search Projects"
-						aria-label="Name"
-					/>
-				</div>
-				<ProjectsFilter @filter="selectedCategory = $event" />
-			</div>
 		</div>
 
 		<!-- Projects grid -->
