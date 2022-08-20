9b56615ae07b111bf2203b8f2c655ecb6d2e8c08
Vkbishu <bishukumar007@gmail.com>
2022-08-20 11:00:50 +0530

    remove load more button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,12 @@
 <script>
 import AppBanner from '@/components/shared/AppBanner';
 import ProjectsGrid from '../components/projects/ProjectsGrid.vue';
-import Button from '../components/reusable/Button.vue';
 
 export default {
 	name: 'Home',
 	components: {
 		AppBanner,
 		ProjectsGrid,
-		Button,
 	},
 	data(){
 		return {
@@ -47,16 +45,6 @@ export default {
 		<!-- Projects -->
 		<ProjectsGrid />
 
-		<!-- Load more projects button -->
-		<div class="mt-10 sm:mt-20 flex justify-center">
-			<router-link
-				to="/projects"
-				class="font-general-medium flex items-center px-6 py-3 rounded-lg shadow-lg hover:shadow-xl bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 text-white text-lg sm:text-xl duration-300"
-				aria-label="More Projects"
-			>
-				<Button title="More Projects" />
-			</router-link>
-		</div>
 	</div>
 </template>
 
